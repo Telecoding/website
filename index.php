b852f871ffe7c33187b5f654a26e7eab12b8f214
Kang <kev.quack@gmail.com>
2015-04-03 18:49:08 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+
+<html lang="MUL" dir="ltr" translate="no">
+
+	<head>
+
+		<meta charset="utf-8">
+		<title>Main page</title>
+		<meta name="description" content="This is the main page.">
+		<meta name="keywords" content="Main page, Site, haha">
+		<link rel="stylesheet" href="/_css/MUL-layout.css" media="all">
+		<link rel="stylesheet" href="/_css/MUL-specified.css" media="all">
+		<style media="all">
+			/* ... */
+		</style>
+		<script src="/_js/main.js"></script>
+		<script>
+			/* ... */
+		</script>
+
+	</head>
+
+	<body>
+
+		<header>
+			<div id="logo">
+				<!-- Big global logo -->
+			</div>
+		</header>
+		<main>
+			<div id="selection">
+				<a href="/de/index.php">Deutsch - German</a>
+				<a href="/en/index.php">English - English</a>
+				<a href="/ru/index.php">Русский - Russian</a>
+				<a href="/ja/index.php">日本語 - Japanese</a>
+				<a href="/zh-Hans/index.php">简体中文 - Simplified Chinese</a>
+			</div>
+		</main>
+		<footer>
+			<div id="footer">
+				CR
+			</div>
+		</footer>
+
+	</body>
+
+</html>
+
+<!-- 
+	Languages:
+	dir: ltr , rtl, ttb, sign language, 
+
+	XXX.int/en/...
+-->
