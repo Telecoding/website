a15643481cd3a2e582ee6578676a722d3647225f
Kang <kev.quack@gmail.com>
2015-04-03 18:51:27 +0200

    Create index.php

---
+++ en/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr" translate="no">
+
+	<head>
+
+		<meta charset="utf-8">
+		<title>Main page</title>
+		<meta name="description" content="This is the main page.">
+		<meta name="keywords" content="Main page, Site, haha">
+		<link rel="stylesheet" href="/_css/en-layout.css" media="all">
+		<link rel="stylesheet" href="/_css/en-specified.css" media="all">
+		<style media="all">
+			/* ... */
+		</style>
+		<script src="/main.js"></script>
+		<script>
+			/* ... */
+		</script>
+
+	</head>
+
+	<body>
+
+		<header>
+			<nav id="global-navigation">
+				<div id="global-logo">
+					<!-- Global logo -->
+				</div>
+				<ul>
+					<!-- Global navigation bar -->
+					<li><a ></a></li>
+				</ul>
+			</nav>
+			<nav id="site-navigation">
+				<ul>
+					<!-- Site navigation bar -->
+					<li><a ></a></li>
+				</ul>
+			</nav>
+			<div id="account">
+				<!-- Account registration -->
+			</div>
+		</header>
+		<main>
+			<nav id="location-on-website">
+				<ul>
+					<li></li>
+				</ul>
+			</nav>
+			Hello, my name is Jason.
+		</main>
+		<footer>
+		</footer>
+
+	</body>
+
+</html>
+
+<!-- 
+	Languages:
+	dir: ltr , rtl, ttb, sign language, 
+
+	XXX.int/en/...
+-->
